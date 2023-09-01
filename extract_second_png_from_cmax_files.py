4f596ee99dbc547f5e595a81459c42a6bad3e83e
90mp11 <90mp11@gmail.com>
2023-09-01 11:45:03 +0100

    Handle zip files directly in output

--- extract_second_png_from_cmax_files.py
+++ extract_second_png_from_cmax_files.py
@@ -1,11 +1,47 @@
 import os
 from PIL import Image
 import io
+import zipfile
 
+# Function 
+# Modifying the main function to include logic for handling .zip files
 def main():
     folder_path = "./raw"
+    temp_folder = "./temp"
     output_path = "./output"
-    print(extract_second_png_from_cmax_files(folder_path, output_path))
+    
+    # Create temp folder if it doesn't exist
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+    
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        
+        # If the file is a .zip file, handle it
+        if file_name.lower().endswith('.zip'):
+            handle_zip_file(file_path, temp_folder)
+            folder_to_process = temp_folder
+        else:
+            folder_to_process = folder_path
+        
+        extract_second_png_from_cmax_files(folder_to_process, output_path)
+    
+    # Cleanup: Remove temporary files
+#    if os.path.exists(temp_folder):
+#        for file_name in os.listdir(temp_folder):
+#            file_path = os.path.join(temp_folder, file_name)
+#            os.remove(file_path)
+
+
+def handle_zip_file(zip_file_path, temp_folder):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        # Filtering out .cmax2 files from the archive
+        cmax2_files = [file for file in zip_ref.namelist() if file.lower().endswith('.cmax2')]
+        
+        # Extracting .cmax2 files to the temporary folder
+        for file in cmax2_files:
+            zip_ref.extract(file, temp_folder)
+
 
 def extract_image(binary_data, start_pos, image_type):
     if image_type == 'JPEG':
